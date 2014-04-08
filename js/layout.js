c9a5a43c4d933b1d370031eafaecaf2e46042db8
dgs360 <dgs360@users.noreply.github.com>
2014-04-08 12:41:18 +0100

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -1,36 +1,37 @@
-// Layout
+// Layout js
+// ===========
 
 $(function(){
-	
 	/* 
-	 * GLOBAL VARS
-		*/
+ 	* GLOBAL VARS
+	*/
 		
 		
 	/*
 	 * CARROUCEL SLIDER
-		*/    
+	*/    
   
-		/// Stop autoplay
-		$('.carousel').each(function(){
+	/// Stop autoplay
+	$('.carousel').each(function(){
   		$(this).carousel({
    			pause: true,
-    		interval: false
+    			interval: false
    		});
-   });
-		$('.carousel').carousel('pause');	
+   	});
+	
+	$('.carousel').carousel('pause');	
 			
-		/// For Mobile - Swipe do scroll para mobile version 
-		$(".carousel-inner").swipe({
-				/* Generic swipe handler for all directions */
-				swipeLeft:function(event, direction, distance, duration, fingerCount){ 
-						$(this).parent().carousel('next');
-				},swipeRight: function(){
-						$(this).parent().carousel('prev');
-				},
-				/* Default is 75px, set to 0 for demo so any distance triggers swipe */
-				threshold:0
-		});
+	/// For Mobile - Swipe do scroll para mobile version 
+	$(".carousel-inner").swipe({
+		///Generic swipe handler for all directions
+		swipeLeft:function(event, direction, distance, duration, fingerCount){ 
+			$(this).parent().carousel('next');
+		},swipeRight: function(){
+			$(this).parent().carousel('prev');
+		},
+		///Default is 75px, set to 0 for demo so any distance triggers swipe
+		threshold:0
+	});
 
 /*
  * End .js
