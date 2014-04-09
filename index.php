bdcdd0ce35dd9f073042a798275c8e1f996f543f
dgs360 <dgs360@users.noreply.github.com>
2014-04-09 17:18:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,32 +9,10 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Title</title>
-		<meta name="keywords" content="">
-		<meta name="description" content="">
-		<meta name="robots" content="index, follow">
-		<meta name="copyright" content="DGS Fotografia e Multimédia">
-		<meta name="author" content="DGS Fotografia e Multimédia">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-
-
-		<link rel="shortcut icon" type="image/png" href="<?=$path_icons;?>icon-logo.png">
-
-		<!-- css -->
-		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-		<link href="css/layout.min.css" rel="stylesheet">
-
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-		    <![endif]-->
-
-		<!-- jQuery -->
-		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-		<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+		<title><?= $row_setup['sitename']; ?></title>
+		<?= $row_setup['meta'];?>
+		<?= $row_setup['css'];?>
+		<?= $row_setup['js'];?>
 	</head>
 	<body>
 		<?php
@@ -70,12 +48,6 @@
 		
 		?>
 
-		<!-- jQuery --> 
-		<script src="js/bootstrap.min.js"></script> 
-		<script src="js/layout.min.js"></script> 
-		<script src="js/plugins/jquery.touchSwipe.min.js"></script> 
-		<script src="js/plugins/jquery.mixitup.min.js"></script> 
-		<script src="js/plugins/jquery.picture.min.js"></script>
 	</body>
 </html>
 <?php
